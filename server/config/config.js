adbbc166f79c93b2580a8cca9627352e09534764
yjmarichal <yjmarichal@gmail.com>
2020-05-18 18:28:42 -0400

    rest server - creando la variable de entorno con la uri de la base de datos

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://express:EWco5wmyohbD8PCG@cluster0-wicav.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
 
